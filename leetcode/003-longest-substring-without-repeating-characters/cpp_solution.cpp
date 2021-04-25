753d3eaffdc2a0d2b17972732f5a935edf5408a0
zhenhuli <zhenhuli@tencent.com>
2021-04-25 15:54:02 +0800

    1

--- leetcode/003-longest-substring-without-repeating-characters/cpp_solution.cpp
+++ leetcode/003-longest-substring-without-repeating-characters/cpp_solution.cpp
@@ -9,7 +9,20 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
+        vector<int> hashTable(256, -1);
+
+        int start = -1;
+        int result = 0;
+        for(int i=0;i<s.size();i+=1) {
+
+            if (hashTable[s[i]] > start) {
+                start = hashTable[s[i]];
+            }
+            hashTable[s[i]] = i;
+            result = max(result, i - start);
+        }
+
+        return result;
     }
 };
 
@@ -17,6 +30,10 @@ public:
 int main() {
     Solution s;
 
-
+    cout << s.lengthOfLongestSubstring("a") << endl;
+    cout << s.lengthOfLongestSubstring("ab") << endl;
+    cout << s.lengthOfLongestSubstring("aba") << endl;
+    cout << s.lengthOfLongestSubstring("abba") << endl;
+    cout << s.lengthOfLongestSubstring("abcdefg") << endl;
     return 0;
 }
